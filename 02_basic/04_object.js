672fef4eb87bb24faef15ca051e236d6abc9df77
Sonu1996kumar <113058566+Sonu1996kumar@users.noreply.github.com>
2023-06-19 10:41:21 +0000

    object2

---
+++ 02_basic/04_object.js
@@ -0,0 +1,58 @@
+//const tinderUser = new object() //singleton
+const tinderUser = {} //non singleton
+
+tinderUser.id = "132kcxnm"
+tinderUser.name = "sammy"
+tinderUser.isLoggedIn = false
+//console.log(tinderUser)
+
+//dono same hi hai bas diff singleton and non singleton ka hai
+
+//object ke ander object
+const regularUser = {
+    email:"soejn@gmail.com",
+    fullname:{
+        userfullname:{
+        firstname:"sonu",
+        lastname: "singh"
+       }
+    }
+}
+
+//console.log(regularUser.fullname.userfullname.firstname);
+
+//merging object
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"a", 4:"b"}
+const obj4 = {5:"a",6:"b"}
+//const obj3 = {obj1,obj2} =>object ke ander object
+// const obj3 = Object.assign(obj1,obj2)
+//const obj3 = Object.assign({},obj1,obj2,obj4) // is trah bhi likh sakte hai {} target hai baki object 
+//isi me return honge nhi to yaad rakhna hoga ki baki sab object obj1 me assign ho rah hai
+//isko bhi kam use karne wale hai 
+//90 % use ye karenge
+const obj3 = {...obj1,...obj2}  
+console.log(obj3);
+
+//jab data base se value ayega to array of object aayega
+const users = [
+    {
+        id: 1,
+        email: "ghvhjg"
+    },
+    {
+        id: 2,
+        email: "ghhjg"
+    },
+    {
+        id: 3,
+        email: "hvhjg"
+    }
+]
+
+//console.log(users[1].email)
+console.log(tinderUser)
+
+console.log(Object.keys(tinderUser));// ab ye array me convert ho gya hai hm ab loop laga sakte hain
+console.log(Object.values(tinderUser))
+console.log(Object.entries(tinderUser)) // kam hi use aata hai
